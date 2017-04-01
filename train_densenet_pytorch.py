922478acf5902de7b71f1988a7beff60b7be3b6a
Brendan Fortuner <bfortuner@gmail.com>
2017-03-31 23:58:49 -0700

    created utils directory with plot and train utils

--- train_densenet_pytorch.py
+++ train_densenet_pytorch.py
@@ -18,45 +18,14 @@ import sys
 import math
 import time
 import string
-import random
-
-import shutil
 
 import setproctitle
 
 from densenet_pytorch import DenseNet
-import make_graph
+import utils.training as train_utils
 
-DATA_PATH='data/'
 CIFAR10_PATH=DATA_PATH+'cifar10/'
-RESULTS_PATH='results/'
-WEIGHTS_PATH='models/'
 
-def get_rand_str(n):
-    return ''.join(random.choices(string.ascii_uppercase + string.digits, k=n))
-
-def save_weights(model, epoch, loss, err, sessionName):
-    weights_fname = sessionName+'-%d-%.3f-%.3f.pth' % (epoch, loss, err)
-    weights_fpath = os.path.join(WEIGHTS_PATH, weights_fname)
-    torch.save({
-            'startEpoch': epoch+1,
-            'loss':loss,
-            'error': err,
-            'sessionName': sessionName,
-            'state_dict': model.state_dict()
-        }, weights_fpath )
-    shutil.copyfile(weights_fpath, WEIGHTS_PATH+'latest.pth')
-
-def load_weights(model, fpath):
-    print("loading weights '{}'".format(fpath))
-    weights = torch.load(fpath)
-    startEpoch = weights['startEpoch']
-    model.load_state_dict(weights['state_dict'])
-    print("loaded weights from session {} (lastEpoch {}, loss {}, error {})"
-          .format(weights['sessionName'], startEpoch-1, weights['loss'], 
-                  weights['error']))
-    return startEpoch
-    
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('--nClasses', type=int, default=10) #CIFAR
@@ -77,11 +46,11 @@ def main():
     args = parser.parse_args()
 
     args.cuda = not args.no_cuda and torch.cuda.is_available()
-    setproctitle.setproctitle(args.save)
+    setproctitle.setproctitle(args.save) #The process name
 
     torch.manual_seed(args.seed)
     if args.cuda:
-        print("CUDA")
+        print("Using CUDA")
         torch.cuda.manual_seed(args.seed)
 
 #    if os.path.exists(args.save):
@@ -119,7 +88,7 @@ def main():
 
     if args.existingWeights:
         print ("Loading existing weights: %s" % args.existingWeights)
-        startEpoch = load_weights(net, args.existingWeights)
+        startEpoch = train_utils.load_weights(net, args.existingWeights)
         endEpoch = startEpoch + args.nEpochs
         print ('Resume training at epoch: {}'.format(startEpoch))
         if os.path.exists(args.save+'train.csv'): #assume test.csv exists
@@ -155,9 +124,9 @@ def main():
     print("Training....")
     for epoch in range(startEpoch, endEpoch+1):
         since = time.time()
-        adjust_opt(args.opt, optimizer, epoch)
-        train(args, epoch, net, trainLoader, optimizer, trainF)
-        test(args, epoch, net, testLoader, optimizer, testF)
+        train_utils.adjust_opt(args.opt, optimizer, epoch)
+        train_utils.train(args, epoch, net, trainLoader, optimizer, trainF)
+        train_utils.test(args, epoch, net, testLoader, optimizer, testF)
         time_elapsed = time.time() - since  
         print('Time {:.0f}m {:.0f}s\n'.format(
             time_elapsed // 60, time_elapsed % 60))
@@ -166,67 +135,6 @@ def main():
 
     trainF.close()
     testF.close()
-
-def train(args, epoch, net, trainLoader, optimizer, trainF):
-    net.train()
-    nProcessed = 0
-    nTrain = len(trainLoader.dataset)
-    for batch_idx, (data, target) in enumerate(trainLoader):
-        if args.cuda:
-            data, target = data.cuda(), target.cuda()
-        data, target = Variable(data), Variable(target)
-        optimizer.zero_grad()
-        output = net(data)
-        loss = F.nll_loss(output, target)
-        # make_graph.save('/tmp/t.dot', loss.creator); assert(False)
-        loss.backward()
-        optimizer.step()
-        nProcessed += len(data)
-        pred = output.data.max(1)[1] # get the index of the max log-probability
-        incorrect = pred.ne(target.data).cpu().sum()
-        err = 100.*incorrect/len(data)
-        partialEpoch = epoch + batch_idx / len(trainLoader) - 1
-#        print('Train Epoch: {:.2f} [{}/{} ({:.0f}%)]\tLoss: {:.6f}\tError: {:.6f}'.format(
-#            partialEpoch, nProcessed, nTrain, 100. * batch_idx / len(trainLoader),
-#            loss.data[0], err))
-        trainF.write('{},{},{}\n'.format(partialEpoch, loss.data[0], err))
-        trainF.flush()
-    save_weights(net, epoch, loss.data[0], err, args.sessionName)
-    print('Epoch {:d}: Train - Loss: {:.6f}\tError: {:.6f}'.format(epoch, loss.data[0], err))
-
-        
-def test(args, epoch, net, testLoader, optimizer, testF):
-    net.eval()
-    test_loss = 0
-    incorrect = 0
-    for data, target in testLoader:
-        if args.cuda:
-            data, target = data.cuda(), target.cuda()
-        data, target = Variable(data, volatile=True), Variable(target)
-        output = net(data)
-        test_loss += F.nll_loss(output, target).data[0]
-        pred = output.data.max(1)[1] # get the index of the max log-probability
-        incorrect += pred.ne(target.data).cpu().sum()
-
-    test_loss = test_loss
-    test_loss /= len(testLoader) # loss function already averages over batch size
-    nTotal = len(testLoader.dataset)
-    err = 100.*incorrect/nTotal
-    print('Test - Loss: {:.4f}, Error: {}/{} ({:.0f}%)'.format(
-        test_loss, incorrect, nTotal, err))
-
-    testF.write('{},{},{}\n'.format(epoch, test_loss, err))
-    testF.flush()
-
-def adjust_opt(optAlg, optimizer, epoch):
-    if optAlg == 'sgd':
-        if epoch < 150: lr = 1e-1
-        elif epoch == 150: lr = 1e-2
-        elif epoch == 225: lr = 1e-3
-        else: return
-
-        for param_group in optimizer.param_groups:
-            param_group['lr'] = lr
 
 if __name__=='__main__':
     main()
---
+++ utils/training.py
@@ -0,0 +1,97 @@
+import argparse
+import torch
+import torchvision.transforms as transforms
+from torchvision.utils import save_image
+import os
+import sys
+import math
+import time
+import string
+import random
+import shutil
+import make_graph
+
+DATA_PATH='data/'
+RESULTS_PATH='results/'
+WEIGHTS_PATH='models/'
+
+def get_rand_str(n):
+    return ''.join(random.choices(string.ascii_uppercase + string.digits, k=n))
+
+def save_weights(model, epoch, loss, err, sessionName):
+    weights_fname = sessionName+'-%d-%.3f-%.3f.pth' % (epoch, loss, err)
+    weights_fpath = os.path.join(WEIGHTS_PATH, weights_fname)
+    torch.save({
+            'startEpoch': epoch+1,
+            'loss':loss,
+            'error': err,
+            'sessionName': sessionName,
+            'state_dict': model.state_dict()
+        }, weights_fpath )
+    shutil.copyfile(weights_fpath, WEIGHTS_PATH+'latest.pth')
+
+def load_weights(model, fpath):
+    print("loading weights '{}'".format(fpath))
+    weights = torch.load(fpath)
+    startEpoch = weights['startEpoch']
+    model.load_state_dict(weights['state_dict'])
+    print("loaded weights from session {} (lastEpoch {}, loss {}, error {})"
+          .format(weights['sessionName'], startEpoch-1, weights['loss'], 
+                  weights['error']))
+    return startEpoch
+  
+def train(epoch, net, trainLoader, optimizer, trainF, sessionName=None):
+    net.train()
+    nProcessed = 0
+    nTrain = len(trainLoader.dataset)
+    for batch_idx, (data, target) in enumerate(trainLoader):
+        data, target = Variable(data.cuda()), Variable(target.cuda())
+        optimizer.zero_grad()
+        output = net(data)
+        loss = F.nll_loss(output, target)
+        # make_graph.save('/tmp/t.dot', loss.creator); assert(False)
+        loss.backward()
+        optimizer.step()
+        nProcessed += len(data)
+        pred = output.data.max(1)[1] # get the index of the max log-probability
+        incorrect = pred.ne(target.data).cpu().sum()
+        err = 100.*incorrect/len(data)
+        partialEpoch = epoch + batch_idx / len(trainLoader) - 1
+#        print('Train Epoch: {:.2f} [{}/{} ({:.0f}%)]\tLoss: {:.6f}\tError: {:.6f}'.format(
+#            partialEpoch, nProcessed, nTrain, 100. * batch_idx / len(trainLoader),
+#            loss.data[0], err))
+        trainF.write('{},{},{}\n'.format(partialEpoch, loss.data[0], err))
+        trainF.flush()
+    save_weights(net, epoch, loss.data[0], err, sessionName)
+    print('Epoch {:d}: Train - Loss: {:.6f}\tError: {:.6f}'.format(epoch, loss.data[0], err))
+    
+def test(epoch, net, testLoader, optimizer, testF):
+    net.eval()
+    test_loss = 0
+    incorrect = 0
+    for data, target in testLoader:
+        data, target = Variable(data.cuda(), volatile=True), Variable(target.cuda())
+        output = net(data)
+        test_loss += F.nll_loss(output, target).data[0]
+        pred = output.data.max(1)[1] # get the index of the max log-probability
+        incorrect += pred.ne(target.data).cpu().sum()
+
+    test_loss = test_loss
+    test_loss /= len(testLoader) # loss function already averages over batch size
+    nTotal = len(testLoader.dataset)
+    err = 100.*incorrect/nTotal
+    print('Test - Loss: {:.4f}, Error: {}/{} ({:.0f}%)'.format(
+        test_loss, incorrect, nTotal, err))
+
+    testF.write('{},{},{}\n'.format(epoch, test_loss, err))
+    testF.flush()
+
+def adjust_opt(optAlg, optimizer, epoch):
+    if optAlg == 'sgd':
+        if epoch < 150: lr = 1e-1
+        elif epoch == 150: lr = 1e-2
+        elif epoch == 225: lr = 1e-3
+        else: return
+
+        for param_group in optimizer.param_groups:
+            param_group['lr'] = lr
